09f2cd10f72118adad08589aaee041939efbc9c1
Ali <ghanimatiali04@gmail.com>
2025-12-30 23:00:12 +0330

    added creating new user  method to signup function

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, redirect, url_for, request
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from . import db
+from .models import User
+from flask_login import login_user, logout_user, login_required
 
 auth = Blueprint("auth", __name__)
 
@@ -11,10 +14,34 @@ def login():
 
 @auth.route("/sign-up", methods=['GET', 'POST'])
 def sign_up():
-    email=request.form.get("email")
-    username=request.form.get("username")
-    password1=request.form.get("password1")
-    password2=request.form.get("password2")
+    if request.method == 'POST':
+        email=request.form.get("email")
+        username=request.form.get("username")
+        password1=request.form.get("password1")
+        password2=request.form.get("password2")
+
+        email_exists = User.query.filter_by(email=email).first()
+        username_exists = User.query.filter_by(username=username).first()
+
+        if email_exists:
+            flash('Email is already in use.', category='error')
+        elif username_exists:
+            flash('Username is already in use.', category='error')
+        elif password1 != password2:
+            flash("Passwords don't match.", category='error')
+        elif len(username) < 2:
+            flash("User name is too short.", category='error')
+        elif len(password1) < 8:
+            flash('Password is too short.', category='error')
+        elif len(email) < 4:
+            flash('Email is invalid.', category='error')
+        else:
+            new_user = User(email=email, username=username, password=password1)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('User created!')
+            return redirect(url_for('views.home'))
+
     return render_template("signup.html")
 
 @auth.route("/log-out")
