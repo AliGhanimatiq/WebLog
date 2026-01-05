57a017942bf54d19572d4c1dc988b93127059b83
Ali <ghanimatiali04@gmail.com>
2026-01-05 14:05:39 +0330

    i forget to coomit login_required section for home page

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from website import create_app
+from flask_login import login_required, current_user
+
 
 if __name__ == "__main__":
     app = create_app()
--- website/views.py
+++ website/views.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, render_template
-from flask_login import login_required, current_user
+from flask_login import  login_required, current_user #restricting user to access home page without logging in
 
 views = Blueprint("views", __name__)
-
-
+ 
+#i forget to commit this section
 @views.route("/")
 @views.route("/home")
 @login_required
 def home():
-    return render_template("home.html", name=current_user.username)
+    return render_template("home.html", name=current_user.username) 
