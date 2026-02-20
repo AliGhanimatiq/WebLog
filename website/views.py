5db37c5ee601de9d8d2d0a20bf2cd97feb51a466
Ali <ghanimatiali04@gmail.com>
2026-02-20 17:53:38 +0330

    added code for create_post.html in views.py file

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import  login_required, current_user #restricting user to access home page without logging in
 
 views = Blueprint("views", __name__)
@@ -8,4 +8,18 @@ views = Blueprint("views", __name__)
 @views.route("/home")
 @login_required
 def home():
-    return render_template("home.html", user=current_user.username) 
+    return render_template("home.html", user=current_user.username)
+
+
+@views.route("/create-post", methods=['GET', 'POST'])
+@login_required
+def create_post():
+    if request.method == "POST":
+        test=request.form.get('text')
+
+        if not text:
+            flash('post cannot be empty', category='error')
+        else:
+            flash("post created!", category='success')
+
+    return render_template('create_post.html', user=current_user)
